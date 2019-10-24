4c3fd05922a27d9d2130030c3e750da7c21a7e1b
Thalesrup <45312085+Thalesrup@users.noreply.github.com>
2019-10-24 15:21:02 -0300

    Update get_key_array_multi.php

--- get_key_array_multi.php
+++ get_key_array_multi.php
@@ -1,8 +1,29 @@
+<?php
 
-    PHP
+     function validar_regra($string)
+    {
+        $tabelaRegimePrevidenciario = array(
+            1 => array('REGIME PRÓPRIO DE PREVIDÊNCIA SOCIAL', 'RPPS'),
+            2 => 'REGIME PRÓPRIO MILITAR',
+            3 => array('REGIME PRÓPRIO EM EXTINÇÃO', 'RPPS Em Extinção'),
+            4 => 'REGIME PRÓPRIO MILITAR EM EXTINÇÃO',
+            5 => array('REGIME GERAL DE PREVIDÊNCIA SOCIAL (INSS)','RGPS')
+        );
+
+        $codigoRegimePrevidenciario = array_search($string, $tabelaRegimePrevidenciario);
+         
+         // Se for encontrada uma chave referente a sring informada ele ira 
+         // retornar um int se não encontrar retorna false e cai na condição abaixo
+         
+         if(is_bool($codigoRegimePrevidenciario)){
+            return valida_array_multidimenssional($string, $tabelaRegimePrevidenciario);
+        }
+        return $codigoRegimePrevidenciario;
+    }
+
     
     // Function Retorna o Indice($codigo) correspondente ao dado que esta sendo buscado dentro da Array-Multidimenssional
-    public function valida_array_multidimenssional($codigoParametro,$arrayTabela)
+    function valida_array_multidimenssional($codigoParametro,$arrayTabela)
     {
         foreach($arrayTabela as $codigo => $valor){
             if(is_array($valor)){
@@ -16,3 +37,5 @@
             }
         }
     }
+
+echo validar_regra('RGPS');
