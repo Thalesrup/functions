a9b53502bf4f603a6ea488dd006b3370325136ca
Thales Ruppenthal <45312085+Thalesrup@users.noreply.github.com>
2023-08-17 08:46:07 -0300

    Update get_key_array_multi.php

--- get_key_array_multi.php
+++ get_key_array_multi.php
@@ -1,41 +1,33 @@
 <?php
 
-     function validar_regra($string)
-    {
-        $tabelaRegimePrevidenciario = array(
-            1 => array('REGIME PRÓPRIO DE PREVIDÊNCIA SOCIAL', 'RPPS'),
-            2 => 'REGIME PRÓPRIO MILITAR',
-            3 => array('REGIME PRÓPRIO EM EXTINÇÃO', 'RPPS Em Extinção'),
-            4 => 'REGIME PRÓPRIO MILITAR EM EXTINÇÃO',
-            5 => array('REGIME GERAL DE PREVIDÊNCIA SOCIAL (INSS)','RGPS')
-        );
+function validarRegra($string)
+{
+    $tabelaRegimePrevidenciario = [
+        1 => ['REGIME PRÓPRIO DE PREVIDÊNCIA SOCIAL', 'RPPS'],
+        2 => 'REGIME PRÓPRIO MILITAR',
+        3 => ['REGIME PRÓPRIO EM EXTINÇÃO', 'RPPS Em Extinção'],
+        4 => 'REGIME PRÓPRIO MILITAR EM EXTINÇÃO',
+        5 => ['REGIME GERAL DE PREVIDÊNCIA SOCIAL (INSS)', 'RGPS']
+    ];
 
-        $codigoRegimePrevidenciario = array_search($string, $tabelaRegimePrevidenciario);
-         
-         // Se for encontrada uma chave referente a sring informada ele ira 
-         // retornar um int se não encontrar retorna false e cai na condição abaixo
-         
-         if(is_bool($codigoRegimePrevidenciario)){
-            return valida_array_multidimenssional($string, $tabelaRegimePrevidenciario);
-        }
-        return $codigoRegimePrevidenciario;
+    $codigoRegimePrevidenciario = array_search($string, array_column($tabelaRegimePrevidenciario, 0), true);
+
+    if ($codigoRegimePrevidenciario === false) {
+        return validaArrayMultidimensional($string, $tabelaRegimePrevidenciario);
     }
-
     
-    // Function Retorna o Indice($codigo) correspondente ao dado que esta sendo buscado dentro da Array-Multidimenssional
-    function valida_array_multidimenssional($codigoParametro,$arrayTabela)
-    {
-        foreach($arrayTabela as $codigo => $valor){
-            if(is_array($valor)){
-                for($i = 0;$i < count($valor);$i++){
-                    $validaArray = array_search($codigoParametro, $valor);
-                    if(is_int($validaArray)){
-                        return $codigo;
-                        break;
-                    }
-                }
+    return $codigoRegimePrevidenciario;
+}
+
+function validaArrayMultidimensional($codigoParametro, $arrayTabela)
+{
+    foreach ($arrayTabela as $codigo => $valor) {
+        if (is_array($valor)) {
+            if (in_array($codigoParametro, $valor)) {
+                return $codigo;
             }
         }
     }
+}
 
-echo validar_regra('RGPS');
+echo validarRegra('RGPS');
