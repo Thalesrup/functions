8d2df9268732f4aea14bdeb4e1b5b5ed695e1eef
Thales Ruppenthal <45312085+Thalesrup@users.noreply.github.com>
2024-08-20 08:58:16 -0300

    Create overview.php

---
+++ container/overview.php
@@ -0,0 +1,17 @@
+$container = new SimpleContainer();
+
+$container->set('logger', new Logger());
+$container->set('database', new DatabaseConnection());
+
+try {
+    $logger = $container->get('logger');
+    $db = $container->get('database');
+} catch (NotFoundException $e) {
+    echo $e->getMessage();
+}
+
+if ($container->has('mailer')) {
+    $mailer = $container->get('mailer');
+} else {
+    echo "Mailer service not found.";
+}
